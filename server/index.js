27627b1b9f532da6d5b9bcd27992f0a23136b2c0
Arthur Tharp <atharp@uci.edu>
2018-09-10 16:51:20 -0700

    uninstalled unecessary dependencies

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ var db = require('../database/database.js');
 var app = express();
 const port = process.env.PORT || 3002;
 
+app.use(express.static(__dirname + '/../client/public'));
+
 app.get('/restaurantInfo/:id', bodyParser.json(), db.findRestaurant);
 
 app.listen(port, ()=>{
-    console.log(' we listening for things');
-})
+  console.log(__dirname, ' the directory name');
+  console.log(' we listening for things');
+});
+
