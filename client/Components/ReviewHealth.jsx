95a069b72951de47462a93329021a7ebbfe2c453
Arthur Tharp <atharp@uci.edu>
2018-09-14 19:42:40 -0700

    modal title bug fix

--- client/Components/ReviewHealth.jsx
+++ client/Components/ReviewHealth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "react-modal";
 
-if (process.env.NODE_ENV !== 'test') ReactModal.setAppElement('#app');
+if (process.env.NODE_ENV !== 'test') Modal.setAppElement('#app');
 
 const textStyle = {
   content: {
